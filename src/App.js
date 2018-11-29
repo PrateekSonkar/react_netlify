ffb21c2f95fe4cc41ffb76ab901838d22001df4a
Prateek Kumar <Prateek.Kumar@synapse.com>
2018-11-29 14:41:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-class App extends Component {
-  render() {
-    return (
+
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+
+    }
+  }
+
+  componentDidMount(){    
+    console.log("ENV FILE : ", process.env)
+    console.log("ENV FILE : ", process.env.REACT_APP_SECRET)
+    axios.get(process.env.REACT_APP_URL)
+    .then(resp => console.log("resp", resp))
+    .catch(err => console.log("Error :", err));
+    
+  }
+
+
+  render(){
+    return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h5>Application JS</h5>
       </div>
-    );
+    )
   }
 }
+
+
+
+
+// class App extends Component {
+//   render() {
+//     return (
+      // <div className="App">
+      //   <h1>Application JS</h1>
+      // </div>
+//     );
+//   }
+// }
 
 export default App;
